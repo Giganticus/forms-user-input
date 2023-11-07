969dbfc879c9835e364a18afa3464779482685aa
Christopher Booth <christopher.booth@juliusbaer.com>
2023-11-07 15:43:20 +0100

    Making a copy of the use of state to get values. Resetting the Login.jsx in preparation for another approach.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,4 @@
-import { useState } from "react";
-
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevState) => ({
-      ...prevState,
-      [identifier]: value,
-    }));
-  }
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -23,26 +10,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => handleInputChange("email", event.target.value)}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" />
         </div>
       </div>
 
